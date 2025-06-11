bc6f0d383aec5b1eff257b997b2df35e7c736345
PaoloVisone <polvis.pv.22@gmail.com>
2025-06-11 13:06:51 +0200

    Refactor ProjectCard component to improve structure and layout

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,9 +3,22 @@ import { Link } from 'react-router-dom';
 const ProjectCard = ({ project }) => {
     return (
         <div className="project-card">
-            <h3>{project.title}</h3>
-            <p>{project.content}</p>
-            <Link to={`/projects/${project.id}`}>View Details</Link>
+            <div className="project-card-content">
+                <h3>{project.title}</h3>
+                <div className="project-spans">
+                    {project.type && <span>{project.type.name}</span>}
+                    {Array.isArray(project.technologies) && project.technologies.map(technology => (
+                        <span key={technology.id}>{technology.name}</span>
+                    ))}
+                    <Link to={`/projects/${project.id}`}>View Details</Link>
+                </div>
+                <p>{project.client}</p>
+            </div>
+            <div className="project-card-image">
+                {project.image && (
+                    <img src={`http://127.0.0.1:8000/storage/${project.image}`} alt={project.title} />
+                )}
+            </div>
         </div>
     );
 };
